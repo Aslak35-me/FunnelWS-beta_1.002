89d61a520c648aed88e2ac19cedd8a9039a2333b
Aslak35-me <aslak35@proton.me>
2025-05-04 22:44:18 +0300

    update install.sh
    update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,14 @@ RESET="\e[0m"
 
 # Sistem tespiti ve kurulum işlemleri
 platform=$(uname)
-
+    echo -e "${CYAN}======================================${RESET}"
 echo -e "${CYAN}Platform tespiti yapılıyor...${RESET}"
+    echo -e "${CYAN}======================================${RESET}"
 if [[ "$platform" == "Linux" ]]; then
     # Linux İçin İşlemler
+    echo -e "${CYAN}=======================================================${RESET}"
     echo -e "${CYAN}Linux sistemi tespit edildi. Sistem güncelleniyor...${RESET}"
+    echo -e "${CYAN}=======================================================${RESET}"
     sudo apt update && sudo apt upgrade -y
     sudo apt update
     sudo apt install golang-go -y
@@ -23,11 +26,14 @@ if [[ "$platform" == "Linux" ]]; then
     else
     echo "[=] PATH zaten ayarlanmış."
     fi
-
+    echo -e "${CYAN}===============================${RESET}"
     echo -e "${CYAN}Gerekli paketler yükleniyor...${RESET}"
+    echo -e "${CYAN}===============================${RESET}"
     sudo apt install -y python3 python3-pip git curl nmap sqlmap hydra whois nikto wapiti
 
+    echo -e "${CYAN}=====================================${RESET}"
     echo -e "${CYAN}Python bağımlılıkları yükleniyor...${RESET}"
+    echo -e "${CYAN}=====================================${RESET}"
     pip3 install colorama requests beautifulsoup4 selenium bs4 dnspython python-whois
     pip install selenium
     pip install webdriver-manager
@@ -36,7 +42,9 @@ if [[ "$platform" == "Linux" ]]; then
     echo "[+] Gereken Python kütüphaneleri yükleniyor..."
     pip install -r requirements.txt
     
+    echo -e "${CYAN}======================================${RESET}"
     echo -e "${CYAN}Güvenlik araçları indiriliyor...${RESET}"
+    echo -e "${CYAN}======================================${RESET}"
     # Araçları GitHub'dan klonla
     git clone https://github.com/sqlmapproject/sqlmap.git scanners/sql_injection/sqlmap
     git clone https://github.com/OWASP/ZAP-ROXY.git scanners/zaproxy
@@ -53,12 +61,16 @@ if [[ "$platform" == "Linux" ]]; then
     go install github.com/tomnomnom/qsreplace@latest
     pip install dirsearch nuclei getJS anew
 
+    echo -e "${CYAN}======================================${RESET}"
     echo -e "${CYAN}Dizin yapısı oluşturuluyor...${RESET}"
+    echo -e "${CYAN}======================================${RESET}"
     mkdir -p logs/sql_logs
     mkdir -p scan_results
     mkdir -p exploit-database/logs
 
+    echo -e "${CYAN}======================================${RESET}"
     echo -e "${GREEN}Kurulum tamamlandı!${RESET}"
+    echo -e "${CYAN}======================================${RESET}"
     echo -e "Çalıştırmak için: ${CYAN}python3 main.py${RESET}"
 
 else
